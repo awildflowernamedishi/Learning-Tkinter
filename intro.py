ffc355626726dc6bad740bf8ae58022d5780ec99
Ishi G <ishita.gupta241@gmail.com>
2023-09-22 12:07:27 +0530

    Modified first program and .gitignore file

--- intro.py
+++ intro.py
@@ -1,2 +1,6 @@
 from tkinter import * #import everything from tkinter
 root=Tk() #main window for widgets
+
+label_wid=Label(root, text="Hello World!") #creating a label widget
+label_wid.pack() #shoving to display on screen
+root.mainloop() #event loop
